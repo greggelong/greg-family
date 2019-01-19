b7c6bf39c4d121a48a7031bf433ed106281d1e8e
Greg K <gkreisman_cep@qq.com>
2019-01-19 12:46:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My family knowledge base
+## fact, rules and clauses in SWI-prolog
+see: SWI-prolog website and Learn Prolog Now
---
+++ gregfamily.pl
@@ -0,0 +1,65 @@
+female(angela).
+female(betty).
+female(dawn).
+female(debbie).
+female(ester).
+female(mary).
+female(nancy).
+female(olivia).
+female(sharon).
+female(stacy).
+female(diane).
+female(donna).
+male(bill).
+male(bud).
+male(dirk).
+male(don).
+male(greg).
+male(kent).
+male(lou).
+male(pat).
+male(paul).
+male(don_jr).
+
+parent(kent,olivia).
+parent(kent,angela).
+parent(kent,greg).
+parent(nancy,greg).
+parent(nancy,angela).
+parent(pat, greg).
+parent(pat, angela).
+
+parent(lou, kent).
+parent(ester, kent).
+parent(bud, nancy).
+parent(bud, betty).
+parent(bud, bill).
+parent(mary, nancy).
+parent(mary, betty).
+parent(mary, bill).
+
+parent(sharon, stacy).
+parent(sharon, paul).
+parent(bill, stacy).
+parent(bill, paul).
+
+parent(don, dirk).
+parent(don, dawn).
+parent(don, debbie).
+parent(don,don_jr).
+parent(don,diane).
+parent(don,donna).
+parent(betty, dirk).
+parent(betty, dawn).
+parent(betty, debbie).
+parent(betty,don_jr).
+parent(betty,diane).
+parent(betty,donna).
+
+
+%% female cousin
+%% ?- female(X),parent(Y,X),parent(Z,greg),parent(W,Y),parent(W,Z), Y \= Z.
+%% ?- female(X),parent(Y,X),parent(Z,greg),parent(W,Y),parent(W,Z), not(Y=Z).
+
+
+
