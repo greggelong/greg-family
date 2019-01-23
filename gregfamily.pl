ef8729e3a3b2707a864f6ef17afd8a462dd7c7c4
Greg K <gkreisman_cep@qq.com>
2019-01-23 20:58:06 +0100

    cousin uncle aunt sibling brother sister

--- gregfamily.pl
+++ gregfamily.pl
@@ -56,6 +56,20 @@ parent(betty,don_jr).
 parent(betty,diane).
 parent(betty,donna).
 
+%% chapter 5 adventure in prolog two way door problem
+
+spouse(ester,lou).
+spouse(mary,bud).
+spouse(betty,don).
+spouse(sharon,bill).
+spouse(nancy,kent).
+spouse(nancy, pat).
+
+married(X,Y):- spouse(X,Y).
+married(X,Y):- spouse(Y,X).
+
+
+
 
 %% female cousin
 %% ?- female(X),parent(Y,X),parent(Z,greg),parent(W,Y),parent(W,Z), Y \= Z.
@@ -79,4 +93,53 @@ is_a_father(X):-
 	male(X).
 	
 grandparent(GP,Y):-
-	parent(X,Y), parent(GP,X).	
+	parent(X,Y), parent(GP,X).
+
+sibling(X,Y):-
+	parent(Z,X),
+	parent(Z,Y), 
+	X\=Y. %% or you can use dif(X,Y)
+
+brother(X,Y):-
+	sibling(X,Y),
+	male(X).
+
+sister(X,Y):-
+	sibling(X,Y),
+	female(X).
+
+
+cousin(X,Y):-
+	parent(Z,X),
+	sibling(Z,W),
+	parent(W,Y).
+
+uncle(X,Y):-
+	parent(Z,Y),
+	sibling(Z,X),
+	male(X).
+
+uncle(X,Y):-
+	aunt(Z,Y),
+	married(X,Z).
+
+		
+
+aunt(X,Y):-
+	parent(Z,Y),
+	sibling(Z,X),
+	female(X).
+
+aunt(X,Y):-
+	uncle(Z,Y),
+	married(X,Z).
+	
+
+/*list_uncle_of(Y):-
+	uncle(X,Y),
+	write(X),
+	nl,
+	fail.
+list_uncle_of(_).
+this makes a endless loop you need to cut.
+*/	
