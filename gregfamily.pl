d0255e3ce51f9a743d40e1eb017588f94a4f3755
Greg K <gkreisman_cep@qq.com>
2019-01-24 19:45:17 +0100

    step father mother bro sis partner add fam

--- gregfamily.pl
+++ gregfamily.pl
@@ -3,7 +3,7 @@ female(betty).
 female(dawn).
 female(debbie).
 female(ester).
-female(kent_s).
+female(ellen).
 female(mary).
 female(nancy).
 female(olivia).
@@ -11,32 +11,51 @@ female(sharon).
 female(stacy).
 female(diane).
 female(donna).
+female(tami).
+female(jennifer).
+female(jen).
+female(marijana).
+female(simone).
+
 male(bill).
 male(bud).
 male(dirk).
 male(don).
 male(greg).
 male(kent).
-male(kent_b).
+male(stuart).
 male(lou).
 male(pat).
 male(paul).
 male(don_jr).
+male(daniel).
+male(joseph).
+male(tim).
+male(kevin).
 
 parent(kent,olivia).
+parent(jennifer,olivia).
+parent(jennifer,simone).
 parent(kent,angela).
 parent(kent,greg).
 parent(nancy,greg).
 parent(nancy,angela).
-parent(pat, greg).
-parent(pat, angela).
+%parent(pat, greg).  make a step father and step mother rule
+%parent(pat, angela).
+parent(pat,tim).
+parent(pat,kevin).
 
 parent(lou, kent).
 parent(ester, kent).
-parent(lou, kent_b).
-parent(ester, kent_b).
-parent(lou, kent_s).
-parent(ester, kent_s).
+parent(lou, stuart).
+parent(ester, stuart).
+parent(lou, ellen).
+parent(ester, ellen).
+
+parent(ellen, daniel).
+parent(ellen, joseph).
+parent(ellen, tami).
+
 
 parent(bud, nancy).
 parent(bud, betty).
@@ -71,9 +90,17 @@ spouse(betty,don).
 spouse(sharon,bill).
 spouse(nancy,kent).
 spouse(nancy, pat).
+spouse(jennifer,kent).
+spouse(jen,angela).
+
+part(marijana,greg).
+
 
 married(X,Y):- spouse(X,Y).
 married(X,Y):- spouse(Y,X).
+
+partner(X,Y):- part(X,Y).
+partner(X,Y):- part(Y,X).
 
 
 
@@ -86,6 +113,12 @@ mother(X,Y):-
 	parent(X,Y),
 	female(X).
 
+step_mother(X,Y):-
+	parent(Z,Y),
+	married(Z,X),
+	female(X),
+	not(mother(X,Y)).	
+
 is_a_mother(X):-
 	parent(X,_),
 	female(X).
@@ -94,6 +127,13 @@ is_a_mother(X):-
 father(X,Y):-
 	parent(X,Y),
 	male(X).
+
+step_father(X,Y):-
+	parent(Z,Y),
+	married(Z,X),
+	male(X),
+	not(father(X,Y)).
+
 
 is_a_father(X):-
 	parent(X,_),
@@ -107,13 +147,28 @@ sibling(X,Y):-
 	parent(Z,Y), 
 	X\=Y. %% or you can use dif(X,Y)
 
+step_sibling(X,Y):-
+	parent(Z,X),
+	married(Z,W),
+	parent(W,Y),
+	X\=Y,
+	not(sibling(X,Y)).
+
 brother(X,Y):-
 	sibling(X,Y),
 	male(X).
 
+step_brother(X,Y):-
+	step_sibling(X,Y),
+	male(X).	
+
 sister(X,Y):-
 	sibling(X,Y),
 	female(X).
+
+step_sister(X,Y):-
+	step_sibling(X,Y),
+	female(X).	
 
 
 cousin(X,Y):-
